bf8f33e34211cd8996d057162699636028eb56a8
Tejaswini-Hegde <tejaswinihegde.97@gmail.com>
2025-08-17 14:32:42 +0530

    Sample code to convert into emojis

--- EmojiConverter.py
+++ EmojiConverter.py
@@ -1,1 +1,16 @@
-print("Hello All")
+message = input("-> ")
+words = message.split(" ")
+print(words)
+emoji = {
+    ":)": "😂",
+    ":(": "😞",
+    "{}": "❤",
+    "*": "⭐",
+    "!": "🔥"
+
+}
+output = ""
+for word in words:
+    output += emoji.get(word, word) + " "
+
+print(output)
